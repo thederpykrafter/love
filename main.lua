8a1664399f589a57bacbb648b7e19c15953b10ff
thederpykrafter <thederpykrafter@gmail.com>
2024-08-12 17:27:37 -0400

    gizmos follow mouse

--- main.lua
+++ main.lua
@@ -1,10 +1,26 @@
 _G.love = require("love")
 
-function _G.debug.drawFPS(x, y)
+---- Debug Stats ----
+_G.debug = {}
+
+function debug.drawFPS(x, y)
 	love.graphics.setColor(0, 0, 0, 1)
 	love.graphics.print("FPS:" .. tostring(love.timer.getFPS()), x, y)
 	love.graphics.rectangle("line", 0, 0, 65, 35)
 end
+---- End Debug Stats ----
+
+---- Random Experiments ----
+_G.gizmos = {}
+
+function gizmos.followMouse()
+	love.graphics.setColor(0, 0, 0, 1)
+	love.graphics.circle("fill", mouseX, mouseY, 5)
+
+	love.graphics.setColor(103 / 255, 41 / 255, 118 / 255, 1)
+	love.graphics.circle("line", mouseX, mouseY, 5)
+end
+---- End Random Experiments ----
 
 ---- Love Functions ----
 function love.load()
@@ -14,13 +30,12 @@ end
 function love.update(dt)
 	_G.width, _G.height = love.graphics.getDimensions()
 	_G.centerX, _G.centerY = width / 2, height / 2
+	_G.mouseX, _G.mouseY = love.mouse.getPosition()
 end
 
 function love.draw()
-	love.graphics.setColor(103 / 255, 41 / 255, 118 / 255, 1)
-
-	love.graphics.setColor(0, 0, 0, 1)
-	love.graphics.circle("line", centerX, centerY, 5)
-
 	debug.drawFPS(10, 10)
+
+	gizmos.followMouse()
 end
+---- End Love Functions ----
